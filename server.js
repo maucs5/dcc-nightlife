ca69ca55907f34b1f816506eac9296bf654626f6
Mau <a@a.a>
2017-03-13 23:32:55 +0800

    Change the URI, for easy deployment

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var app = express();
 require('dotenv').load();
 require('./app/config/passport')(passport);
 
-mongoose.connect(process.env.MONGO_URI);
+mongoose.connect(process.env.MONGODB_URI);
 mongoose.Promise = global.Promise;
 
 app.set('view engine', 'pug')
